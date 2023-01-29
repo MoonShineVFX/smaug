2d31a732dffe8f82a419a0eaee80ab9eeacfef7f
Noflame Lin <linjuang@gmail.com>
2023-01-30 04:17:44 +0800

    wip: add seed data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,7 @@
-import { createId } from '@paralleldrive/cuid2';
-import { PrismaClient, Role, UserType } from '@prisma/client'
 import { faker } from '@faker-js/faker/locale/zh_TW'
+import { PrismaClient, Role, UserType, Representation, RepresentationFormat, RepresentationType } from '@prisma/client'
+import { createId } from '@paralleldrive/cuid2'
+import { connect } from 'http2'
 
 const prisma = new PrismaClient()
 async function main() {
@@ -18,6 +19,18 @@ async function main() {
         ['TAG_DELETE', '刪除標籤'],
     ]
 
+    // 刪除現有資料
+    await prisma.$transaction([
+        prisma.permission.deleteMany({}),
+        prisma.representation.deleteMany({}),
+        prisma.asset.deleteMany({}),
+        prisma.category.deleteMany({}),
+        prisma.user.deleteMany({})
+    ])
+
+
+    // 建立所有資料
+    // 建立權限
     await prisma.permission.createMany({
         data: permission.map((arg) => {
             return {
@@ -27,7 +40,6 @@ async function main() {
             }
         })
     })
-
 
     //create 50 user
     const usersData = Array.from({ length: 50 }).map((_, i) => {
@@ -40,20 +52,20 @@ async function main() {
             createAt: faker.date.past(),
         }
     })
-    await prisma.user.createMany(
-        { data: usersData }
-    )
 
-    const rootId = createId()
+    const users = await prisma.user.createMany({ data: usersData })
+    console.log(`users created`)
+
+
     // Create Root Category
-    await prisma.category.create({
+    const rootId = createId()
+    const create_root = prisma.category.create({
         data: {
             id: rootId,
             name: "Root",
             createAt: faker.date.past(),
         }
     })
-
     // Create 20 categories
     const categoriesLv1 = Array.from({ length: 4 }).map((_, i) => {
         return {
@@ -67,26 +79,82 @@ async function main() {
     const categoriesLv2 = Array.from({ length: 16 }).map((_, i) => {
         return {
             id: createId(),
-            parentId: categoriesLv1[Math.trunc(i/4)].id,
+            parentId: categoriesLv1[Math.trunc(i / 4)].id,
             name: faker.commerce.productName(),
             createAt: faker.date.past(),
         }
     })
 
     const categories = [...categoriesLv1, ...categoriesLv2]
+    const create_cate_tree = prisma.category.createMany({ data: categories })
+    await prisma.$transaction([create_root, create_cate_tree])
+    console.log(`categories created`)
 
-    await prisma.category.createMany({data:categories})
 
     // Create 100 assets
-    const assets = Array.from({ length: 100 }).map((_, i) => {
-        return {
+    let creators = usersData.splice(0, 40)
+    const assetsData = Array.from({ length: 100 }).map((_, i) => {
+        let creator = creators[Math.floor(Math.random() * 40)]
+        // console.log(`creator id: ${creator.id}`)
+        let asset = {
             id: createId(),
             name: faker.commerce.productName(),
-            categoryId: categories[Math.trunc(i/5)].id,
-
+            categoryId: categories[Math.trunc(i / 5)].id,
+            creatorId: creator.id,
             createAt: faker.date.past(),
         }
+        // console.log(`asset: ${asset}`)
+        return asset
     })
+    await prisma.asset.createMany({ data: assetsData })
+    console.log(`assets created`)
+
+
+    // Create 50 representations ->preview
+    await prisma.representation.createMany({
+        data: Array.from({ length: 50 }).map((_, i) => {
+            return {
+                id: createId(),
+                path: faker.image.imageUrl(320, 160),
+                assetId: assetsData[i].id,
+                format: RepresentationFormat.IMG,
+                type: RepresentationType.PREVIEW,
+                uploaderId: assetsData[i].creatorId,
+                createAt: faker.date.past()
+            }
+        })
+    })
+    console.log(`representations PREVIEW created`)
+
+
+    // Create 25 representations ->model
+    for (let i = 0; i < 25; i++) {
+        let texture = await prisma.representation.create({
+            data: {
+                id: createId(),
+                path: faker.image.imageUrl(320, 160),
+                assetId: assetsData[i].id,
+                format: RepresentationFormat.IMG,
+                type: RepresentationType.TEXTURE,
+                uploaderId: assetsData[i].creatorId,
+                createAt: faker.date.past()
+            }
+        })
+
+        let asset = await prisma.representation.create({
+            data: {
+                id: createId(),
+                path: faker.image.imageUrl(320, 160),
+                assetId: assetsData[i].id,
+                format: RepresentationFormat.MAX,
+                type: RepresentationType.MODEL,
+                uploaderId: assetsData[i].creatorId,
+                createAt: faker.date.past(),
+                linkTo:{connect:{id: texture.id}}
+            }
+        })
+        console.log(`texture: ${texture.id} link by asset: ${asset.id}`)
+    }
 }
 
 main()
