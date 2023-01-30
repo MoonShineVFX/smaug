90cad51701cabff3ab99a327d6aa1e2403823ff5
Noflame Lin <linjuang@gmail.com>
2023-01-30 12:52:24 +0800

    modify: arrang element

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,11 @@ import Header from '../components/Header';
 
 export default function Home() {
   return (
-    <Header>
-        <Typography variant="h1" component="h2" gutterBottom>
-            Welcome to Next.js!
-        </Typography>
-    </Header>
+    <>
+    <Header/>
+    <Typography variant="h1" component="h2" gutterBottom>
+      Welcome to Next.js!
+    </Typography>
+    </>
   )
 }
