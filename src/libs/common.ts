bb5a2289ebee5f4d90b8406c777e375f7a512da4
Noflame Lin <linjuang@gmail.com>
2023-02-01 20:49:39 +0800

    chore: add token settings

--- src/libs/common.ts
+++ src/libs/common.ts
@@ -1,34 +1,40 @@
 export type UserInfo = {
-    id: string;
-    name: string;
-    email: string;
-    picture: string;
+  id: string;
+  name: string;
+  email: string;
+  picture: string;
 }
 
 // db init usage
 export const permission = [
-    ['CATEGORY_CREATE', '建立分類'],
-    ['CATEGORY_UPDATE', '更新分類'],
-    ['CATEGORY_DELETE', '刪除分類'],
-    ['ASSET_CREATE', '建立資源'],
-    ['ASSET_UPDATE', '更新資源'],
-    ['ASSET_DELETE', '刪除資源'],
-    ['ASSET_DOWNLOAD', '下載資源'],
-    ['TAG_CREATE', '建立標籤'],
-    ['TAG_UPDATE', '更新標籤'],
-    ['TAG_DELETE', '刪除標籤'],
+  ['CATEGORY_CREATE', '建立分類'],
+  ['CATEGORY_UPDATE', '更新分類'],
+  ['CATEGORY_DELETE', '刪除分類'],
+  ['ASSET_CREATE', '建立資源'],
+  ['ASSET_UPDATE', '更新資源'],
+  ['ASSET_DELETE', '刪除資源'],
+  ['ASSET_DOWNLOAD', '下載資源'],
+  ['TAG_CREATE', '建立標籤'],
+  ['TAG_UPDATE', '更新標籤'],
+  ['TAG_DELETE', '刪除標籤'],
 ]
 
 export const rolesData = [{
-    roleName: 'Admin',
-    permissions: ['CATEGORY_CREATE', 'CATEGORY_UPDATE', 'CATEGORY_DELETE', 'ASSET_CREATE', 'ASSET_UPDATE', 'ASSET_DELETE', 'ASSET_DOWNLOAD', 'TAG_CREATE', 'TAG_UPDATE', 'TAG_DELETE']
+  roleName: 'Admin',
+  permissions: ['CATEGORY_CREATE', 'CATEGORY_UPDATE', 'CATEGORY_DELETE', 'ASSET_CREATE', 'ASSET_UPDATE', 'ASSET_DELETE', 'ASSET_DOWNLOAD', 'TAG_CREATE', 'TAG_UPDATE', 'TAG_DELETE']
 },
 {
-    roleName: 'Creator',
-    permissions: ['ASSET_CREATE', 'ASSET_UPDATE', 'ASSET_DELETE', 'ASSET_DOWNLOAD', 'TAG_CREATE', 'TAG_UPDATE', 'TAG_DELETE']
+  roleName: 'Creator',
+  permissions: ['ASSET_CREATE', 'ASSET_UPDATE', 'ASSET_DELETE', 'ASSET_DOWNLOAD', 'TAG_CREATE', 'TAG_UPDATE', 'TAG_DELETE']
 
 },
 {
-    roleName: 'User',
-    permissions: ['ASSET_DOWNLOAD', 'TAG_CREATE', 'TAG_UPDATE', 'TAG_DELETE']
+  roleName: 'User',
+  permissions: ['ASSET_DOWNLOAD', 'TAG_CREATE', 'TAG_UPDATE', 'TAG_DELETE']
 }]
+
+export const settings = {
+  SALT_ROUNDS: process.env.SALT_ROUNDS,
+  TOKEN_PER_USER: Number(process.env.TOKEN_PER_USER),
+  TOKEN_EXPIRE_TIME: process.env.TOKEN_EXPIRE_TIME,
+}
