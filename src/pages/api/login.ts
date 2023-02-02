79c3fffe9c0fee93b6f0782acd6236efdefe1a5a
Noflame Lin <linjuang@gmail.com>
2023-02-03 04:14:02 +0800

    modify: return token in authorization

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,31 +1,32 @@
+import cookie from 'cookie'
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { ILoginedReturn } from '../../libs/server/auth';
+import { authenticate } from '../../libs/server/auth'
 
-type ResponseData = {
-    message: string,
-    data: ILoginedReturn | null
-}
-
-import { auth } from '../../libs/server/auth'
-
-function login(req: NextApiRequest, resp: NextApiResponse<ResponseData>): void {
-    const auth_str = req.headers.authorization;
-    if (auth_str == null) {
-        resp.status(401).json({ message: "please provide username and password", data: null })
-        return;
-    }
-    const auth_data = auth_str.split(' ')[1]
-    const credentials = Buffer.from(auth_data, 'base64').toString('ascii');
-    const [username, password] = credentials.split(':');
-    const user = auth({ username: username, password: password });
-    if (user) {
-        resp.status(200).json({ message: "", data: user });
-        return;
-    } else {
-        resp.status(401).json({ message: "Invalid credentials", data: null })
-    }
-
+async function handleLogin(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+  const auth_str = req.headers.authorization;
+  if (auth_str == null) {
+    res.status(401).json({ message: "please provide username and password", data: null })
     return;
+  }
+  const auth_data = auth_str.split(' ')[1]
+  const credentials = Buffer.from(auth_data, 'base64').toString('ascii');
+  const [username, password] = credentials.split(':');
+  const token = await authenticate({ username: username, password: password })
+  if (token) {
+    // res.setHeader('Set-Cookie', cookie.serialize('authToken', token, {
+    //   httpOnly: true,
+    //   secure: process.env.NODE_ENV !== 'development',
+    //   sameSite: 'strict',
+    //   path: '/',
+    //   maxAge: 3600,
+    // }))
+    res.setHeader('Authorization', `Bearer ${token}`)
+    res.status(200).json({ message: 'welcome' });
+    return;
+  } else {
+    res.status(401).json({ message: "invalid username or password", data: null })
+  }
+  return;
 }
 
-export default login;
+export default handleLogin;
