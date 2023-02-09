33311fcc2972ba45caa8debcddb648c5030fc71f
wood.wu <kilokingw@gmail.com>
2023-02-09 19:00:51 +0800

    Wip side drawer data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,13 @@ import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
 import ListSubheader from '@mui/material/ListSubheader';
+import Typography from '@mui/material/Typography';
 import Image from 'next/image'
 import img1 from '../public/images/p1.jpg'
 import Drawer from '@mui/material/Drawer';
 import Toolbar from '@mui/material/Toolbar';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 export default function Home() {
   const [open, setOpen] = React.useState(false);
   const toggleDrawer = () => {
@@ -34,17 +36,25 @@ export default function Home() {
       >
       </Toolbar>
         <img src="https://ddinktqu5prvc.cloudfront.net/rmyxk/Grid_render_thumb_thumb.jpg" alt="" />
-        <div>Sea star</div>
-        <div>title</div>
-        <div>tag</div>
-        <div>tag</div>
-        <div>tag</div>
-        <div>tag</div>
-        <div>tag</div>
+        <Box sx={{ p:2}}>
+          <Typography variant="h4" gutterBottom>
+            Vegetables
+          </Typography>
+          <Typography variant="caption" display="block" gutterBottom>
+            3D Assets
+          </Typography>
+        </Box>
+
+        <Box sx={{p:2}}>
+          data info 
+        </Box>
+        <Box sx={{display:'flex',position:'absolute',bottom:0,width:'100%',justifyContent:'center', p:2, bgcolor:"#202020"}}>
+          <Button variant="contained" sx={{m:1, width: .5}}>Download</Button>
+        </Box>
         
       </Drawer>
-      <ImageList sx={{}}  cols={4} gap={8}  sx={{mx:2 , my:2}} variant="standad" >
-        <ImageListItem key="Subheader" cols={4}>
+      <ImageList sx={{}}  cols={5} gap={8}  sx={{mx:2 , my:2}} variant="standad" >
+        <ImageListItem key="Subheader" cols={5}>
           <ListSubheader component="div">3D Assets</ListSubheader>
         </ImageListItem>
         {modalItemData2.map((item) => {
