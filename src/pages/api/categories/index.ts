e6990da3f7be156c1d491f3aaa59970582201fc7
Noflame Lin <linjuang@gmail.com>
2023-02-20 17:40:16 +0800

    update: finish categories get basically

--- src/pages/api/categories/index.ts
+++ src/pages/api/categories/index.ts
@@ -1,10 +1,31 @@
+import crypto from 'crypto';
 import type { NextApiRequest, NextApiResponse } from 'next'
-import type { Category } from '@prisma/client';
+import type { Category, Menu } from '@prisma/client';
 import { prisma } from '../../../libs/server/prisma';
 
+type CategoryReturnItem = {
+  id: string;
+  name: string;
+  parentId: string;
+}
 
-export default async function handlerCategories(req: NextApiRequest, res: NextApiResponse<Category[] | any>): Promise<void> {
-  // return categories and it's assets by category id
+
+type CategoryItem = {
+  id: string;
+  name: string;
+  children: CategoryItem[];
+}
+
+
+type CategoryGetResponse = {
+  id: string;
+  name: string;
+  iconName: string;
+  children: CategoryItem[];
+
+}
+export default async function handlerCategories(req: NextApiRequest, res: NextApiResponse<CategoryGetResponse | any>): Promise<void> {
+
   if (req.method === undefined) {
     res.status(405).json({ message: "Method not allowed" })
     return;
@@ -22,38 +43,32 @@ export default async function handlerCategories(req: NextApiRequest, res: NextAp
 
 }
 
-async function handleGet(req: NextApiRequest, res: NextApiResponse<Category[] | any>): Promise<void> {
+
+function hashString(str: string): string {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    const chr = str.charCodeAt(i);
+    hash = (hash << 5) - hash + chr;
+    hash |= 0;
+  }
+  return hash.toString(16);
+}
+
+
+async function handleGet(req: NextApiRequest, res: NextApiResponse<CategoryGetResponse[] | any>): Promise<void> {
   const { id, name } = req.query
 
   const returnField = {
     select: {
       id: true,
       name: true,
-      _count: {
-        select: {
-          assets: true,
-        }
-      },
-      children: {
-        where: {
-          isDeleted: false,
-        },
-        select: {
-          id: true,
-          name: true,
-          _count: {
-            select: {
-              assets: true,
-            }
-          }
-        }
-      }
+      parentId: true
     }
   }
 
   let queryFilter = {
     where: {
-      AND: {} as { [key: string]: { equals: any; } }
+      AND: {} as { [key: string]: { equals: Boolean | string; } }
     }
   };
 
@@ -61,6 +76,7 @@ async function handleGet(req: NextApiRequest, res: NextApiResponse<Category[] | 
     where: {
       AND: {
         isDeleted: { equals: false },
+        isVisible: { equals: true },
       }
     }
   }
@@ -82,13 +98,64 @@ async function handleGet(req: NextApiRequest, res: NextApiResponse<Category[] | 
     }
   }
 
-  const categroies = await queryFunc(
+  const categroies: CategoryReturnItem[] = await queryFunc(
     {
       ...queryFilter,
       ...returnField
     }
   )
-  res.status(200).json(categroies)
+
+  // get menu "category"
+  const menuCategory = await prisma.menu.findUnique({
+    where: {
+      name: "Categories"
+    },
+  })
+
+  if (!menuCategory) {
+    res.status(404).json({ message: "Menu Categories not found" })
+    return
+  }
+
+  // 把 category 整理成 id 為 key 值為 CategoryItem 的物件
+  let categoryDict = {} as { [key: string]: CategoryItem }
+  categroies.forEach((category) => {
+    categoryDict[category.id] = {
+      id: category.id,
+      name: category.name,
+      children: [],
+    }
+  })
+  // 取的 Root 的 id
+  let rootId = ''
+  for (let key in categoryDict) {
+    if (categoryDict[key].name === 'Root') {
+      rootId = key
+      break
+    }
+  }
+
+  // 利用 categoryDict 整理出 categoryTree，去除掉 category 為 Root 那一層
+  let categoryTree = [] as CategoryItem[]
+  categroies.forEach((category) => {
+    if (category.id !== rootId) {
+      if (category.parentId === rootId) {
+        categoryTree.push(categoryDict[category.id])
+      } else {
+        categoryDict[category.parentId].children.push(categoryDict[category.id])
+      }
+    }
+  })
+
+
+  let categoryReturn = {
+    id: hashString(menuCategory.name),
+    name: menuCategory.name,
+    iconName: menuCategory.iconName,
+    children: categoryTree,
+  }
+
+  res.status(200).json(categoryReturn)
   return
 }
 
