1077d9829f6e60ec0ada3a33587179b6037c0f22
Noflame Lin <linjuang@gmail.com>
2023-08-17 17:54:03 +0800

    Simplify UserProfile to only support basic logout functionality

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -1,7 +1,8 @@
 import Raect, { useState, useRef, useEffect } from 'react';
-import { Avatar, Stack, Button, Tooltip, IconButton, Container, Popover, Typography, Popper, Grow, Paper, ClickAwayListener, Menu, MenuList, MenuItem, Box, TextField } from '@mui/material';
+import { Avatar, Button, Tooltip, IconButton, Typography, Paper, ClickAwayListener, Menu, MenuList, MenuItem, Box, TextField } from '@mui/material';
 import { UserDisplayInfo } from '../libs/types';
-import React from 'react';
+import { OutLineButton } from './basic';
+
 
 interface UserProfileProps {
   userProfile: UserDisplayInfo
@@ -10,13 +11,14 @@ interface UserProfileProps {
 }
 
 function UserProfile({ userProfile, logoutHandler, children }: UserProfileProps): JSX.Element {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
+  const [anchorRef, setAnchorRef] = useState<null | HTMLButtonElement>(null);
+  const open = Boolean(anchorRef);
+
   // const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
   //     setAnchorEl(event.currentTarget);
   // };
   // const handleClose = () => {
-  //     setAnchorEl(null);
+  //   setAnchorRef(null);
   // };
 
   // function handleListKeyDown(event: React.KeyboardEvent) {
@@ -28,63 +30,39 @@ function UserProfile({ userProfile, logoutHandler, children }: UserProfileProps)
   // };
 
 
-  // const Logged_menu = () =>{
-  //     return(
-  //         <Paper>
-  //         <MenuList
-  //             autoFocusItem={open}
-  //             id="composition-menu"
-  //             aria-labelledby="composition-button"
-  //         >
-  //             <MenuItem onClick={handleClose}>Profile</MenuItem>
-  //             <MenuItem onClick={handleClose}>My account</MenuItem>
-  //             <MenuItem onClick={handleClose}>Logout</MenuItem>
-  //         </MenuList>
+  // const LoginedMenu = () => {
+  //   return (
+  //     <Paper>
+  //       <MenuList
+  //         autoFocusItem={open}
+  //         id="composition-menu"
+  //         aria-labelledby="composition-button"
+  //       >
+  //         <MenuItem onClick={handleClose}>Profile</MenuItem>
+  //         <MenuItem onClick={handleClose}>My account</MenuItem>
+  //         <MenuItem onClick={handleClose}>Logout</MenuItem>
+  //       </MenuList>
   //     </Paper>
-  //     )
+  //   )
   // }
 
 
   return (
-    <React.Fragment>
-      <Tooltip title="Account settings">
+    <>
+      <OutLineButton> Sign-out</OutLineButton>
+      <Tooltip title="Account infomation">
         <IconButton
           // onClick={handleClick}
           size="small"
           sx={{ ml: 2 }}
           aria-controls={open ? 'account-menu' : undefined}
-          aria-haspopup="true"
+          aria-haspopup="menu"
           aria-expanded={open ? 'true' : undefined}
         >
           <Avatar sx={{ width: 48, height: 48, m: 1 }} src={userProfile.picture}>{userProfile.name}</Avatar>
         </IconButton>
       </Tooltip>
-
-      <Button
-        sx={{ m: 0 }}
-        id="basic-button"
-        // ref={anchorRef}
-        aria-controls={open ? 'basic-menu' : undefined}
-        aria-expanded={open ? 'true' : undefined}
-        aria-haspopup="true"
-        onClick={logoutHandler}
-
-      > Log-out</Button>
-
-      {/* <Menu
-                open={open}
-                anchorEl={anchorEl}
-                role={undefined}
-                onClose={handleClose}
-
-                id="basic-menu"
-                MenuListProps={{
-                    'aria-labelledby': 'basic-button',
-                }}
-            >
-                {userProfile.id !== '' ? <Logged_menu /> : <Loggin_form />}
-            </Menu> */}
-    </React.Fragment>
+    </>
   );
 }
 
