348821b59e0d540bec1269f3ecd1c99c62babdf8
Noflame Lin <linjuang@gmail.com>
2023-08-21 12:48:56 +0800

    update assetDetai api

--- src/pages/api/assets/[assetId].ts
+++ src/pages/api/assets/[assetId].ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import type { Asset } from '@prisma/client';
-import {  RepresentationType, Prisma } from '@prisma/client';
+import { RepresentationType, Prisma } from '@prisma/client';
 import { prisma } from '../../../libs/server/prisma';
 import util from '../../../utility/util';
 import { AssetDetails } from '../../../libs/types';
@@ -26,110 +26,108 @@ export default async function handlerAsset(req: NextApiRequest, res: NextApiResp
 }
 
 async function handleGet(req: NextApiRequest, res: NextApiResponse<Asset[] | any>): Promise<void> {
-    const selectField = {
+  const selectField = {
+    select: {
+      id: true,
+      name: true,
+      categoryId: true,
+      tags: {
         select: {
-            id: true,
-            name: true,
-            categoryId: true,
-            tags: {
-                select: {
-                    id: true,
-                    name: true,
-                },
-            },
-            creator: {
-                select: {
-                    name: true,
-                },
-            },
-            updateAt: true,
-            createAt: true,
-          }
-      }
-    const { assetId } = req.query;
-   
-    const asset = await prisma.asset.findFirst(
-        {
-            where: {
-                id: {
-                    in: assetId
-                },
-            },
-            ...selectField
-        }
-      )
-    if (asset !== null) {
-      const categories = await prisma.category.findMany({select: {id: true, parentId: true, name: true}})
-      const representations = await prisma.representation.findMany({where: {assetId: {in: assetId}}})
-
-      type Category = {
-        id: string
-        parentId: string | null
-        name: string | null
-      }
-
-      const cate_dict : { [id: string] : Category; } = {};
-
-      categories.forEach((element, index) => {
-        cate_dict[element.id] = element;
-      });
-
-      const getCategoryList = (categoryId: string, categoryList: string | null): string => {
-        const cate = cate_dict[categoryId]
-        const newList = (categoryList == null) ? cate.name : `${cate.name}\\${categoryList}`
-
-        if(cate !== undefined && cate.parentId !== null)
-          return getCategoryList(cate.parentId, newList);
-        else
-          return newList == null? "" : newList;
-      };
-      const categoryList = getCategoryList(asset.categoryId, "")
-
-      const assetReturn: AssetDetails = { 
-              id: asset.id, 
-              preview: "",
-              name: asset.name,
-              categoryList: categoryList,
-              updateAt: asset.updateAt,
-              createAt: asset.createAt,
-              creator: asset.creator.name,
-              tags: asset.tags,
-              renders: new Array(),
-              downloads: new Array(),
-          }
-
-      representations.forEach((element, index) => {
-        switch(element.type) { 
-          case RepresentationType.PREVIEW: { 
-            if (element.path !== null) assetReturn.preview = element.path; 
-            break; 
-          } 
-          case RepresentationType.RENDER: { 
-            const render = {
-              id: element.id,
-              name: element.name,
-              path: element.path,
-            }
-            assetReturn.renders.push(render)
-            break; 
-          } 
-          case RepresentationType.MODEL: { 
-            const download = {
-              id: element.id,
-              name: element.name,
-              format: element.format,
-              fileSize: formatBytes(element.fileSize ? element.fileSize : 0),
-            }
-            assetReturn.downloads.push(download)
-            break; 
-          } 
-          default: { 
-             break; 
-          } 
-        }
-      });
-
-      res.status(200).json(assetReturn)
+          id: true,
+          name: true,
+        },
+      },
+      creator: {
+        select: {
+          name: true,
+        },
+      },
+      updateAt: true,
+      createAt: true,
     }
-    res.status(404).json({ message: "Asset not found" })
   }
+
+  const assetId = typeof req.query.assetId === 'string' ? req.query.assetId : undefined;
+
+  const asset = await prisma.asset.findUnique(
+    {
+      where: { id: assetId },
+      ...selectField
+    }
+  )
+  if (asset !== null) {
+    const assetIds: string[] = assetId ? [assetId] : ['']
+    const categories = await prisma.category.findMany({ select: { id: true, parentId: true, name: true } })
+    const representations = await prisma.representation.findMany({ where: { assetId: { in: assetIds } } })
+
+    type Category = {
+      id: number,
+      parentId: number | null
+      name: string | null
+    }
+
+    const cate_dict: { [id: number]: Category; } = {};
+
+    categories.forEach((element, index) => {
+      cate_dict[element.id] = element;
+    });
+
+    const getCategoryList = (categoryId: number, categoryList: string | null): string => {
+      const cate = cate_dict[categoryId]
+      const newList = (categoryList == null) ? cate.name : `${cate.name}\\${categoryList}`
+
+      if (cate !== undefined && cate.parentId !== null)
+        return getCategoryList(cate.parentId, newList);
+      else
+        return newList == null ? "" : newList;
+    };
+    const categoryList = getCategoryList(asset.categoryId, "")
+
+    const assetReturn: AssetDetails = {
+      id: asset.id,
+      preview: "",
+      name: asset.name,
+      categoryList: categoryList,
+      updateAt: asset.updateAt,
+      createAt: asset.createAt,
+      creator: asset.creator.name,
+      tags: asset.tags,
+      renders: new Array(),
+      downloads: new Array(),
+    }
+
+    representations.forEach((element, index) => {
+      switch (element.type) {
+        case RepresentationType.PREVIEW: {
+          if (element.path !== null) assetReturn.preview = element.path;
+          break;
+        }
+        case RepresentationType.RENDER: {
+          const render = {
+            id: element.id,
+            name: element.name,
+            path: element.path ? element.path: '',
+          }
+          assetReturn.renders.push(render)
+          break;
+        }
+        case RepresentationType.MODEL: {
+          const download = {
+            id: element.id,
+            name: element.name,
+            format: element.format,
+            fileSize: formatBytes(element.fileSize ? element.fileSize : 0),
+          }
+          assetReturn.downloads.push(download)
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    });
+
+    res.status(200).json(assetReturn)
+  }
+  res.status(404).json({ message: "Asset not found" })
+}
