53466695f145e0e281e81fbc8c0bb19437c19ede
Noflame Lin <linjuang@gmail.com>
2023-08-21 19:45:16 +0800

    repair tests case

--- src/__tests__/pages/api/assets/index.test.ts
+++ src/__tests__/pages/api/assets/index.test.ts
@@ -1,23 +1,23 @@
 import { createMocks } from 'node-mocks-http';
 import { PrismaClient, Asset } from '@prisma/client';
 import handlerAsset from '../../../../pages/api/assets'
+import { prismaMock } from '../../../../singleton';
 
-
-let prisma: PrismaClient;
+// let prisma: PrismaClient;
 
 beforeEach(async () => {
-  prisma = new PrismaClient();
+  // prisma = new PrismaClient();
 });
 
 afterEach(async () => {
-  await prisma.authToken.deleteMany({});
-  await prisma.$disconnect();
+  await prismaMock.authToken.deleteMany({});
+  await prismaMock.$disconnect();
 })
 
 describe('Assets API', () => {
 
-  test('return assets list', async () => {
-    const { req, res } = createMocks({ method: 'GET' });
+  test('return assets list of a given category id', async () => {
+    const { req, res } = createMocks({ method: 'GET', query:{cid: 3} });
 
     await handlerAsset(req, res);
     expect(res._getStatusCode()).toBe(200);
