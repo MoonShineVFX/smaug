2ebf11d2a683283c46731e718a5d585708abee69
Noflame Lin <linjuang@gmail.com>
2023-09-23 11:36:38 +0800

    basic component added
    Loading -> CircularIndeterminate
    No Data -> EmptyState
    Error -> ErrorState

--- src/components/basic.tsx
+++ src/components/basic.tsx
@@ -1,11 +1,37 @@
-import { Button } from "@mui/material";
+import { Box, Button, CircularProgress, Typography } from "@mui/material";
 import { styled } from '@mui/material/styles';
 
 export const OutLineButton = styled(Button)(({ theme }) => ({
-    color: "#bbb",
-    borderColor: "#bbb",
-    '&:hover': {
-      backgroundColor: "#fff",
-      color: "#000",
-    },
-  }));
+  color: "#bbb",
+  borderColor: "#bbb",
+  '&:hover': {
+    backgroundColor: "#fff",
+    color: "#000",
+  },
+}));
+
+
+export const CircularIndeterminate = () => {
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CircularProgress />
+    </Box>
+  );
+}
+
+
+export const EmptyState = () => {
+  return (
+    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh'}}>
+      <Typography>No Asset In This Category</Typography>
+    </Box>
+  )
+}
+
+export const ErrorState = (err: any) => {
+  return (
+    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
+      <Typography>{`${err.message}`}</Typography>
+    </Box>
+  )
+}
