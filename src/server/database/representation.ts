5906e3d8512771b6e460b453badfebdcb3c2ccfe
Noflame Lin <linjuang@gmail.com>
2023-10-23 09:29:59 +0800

    add representation tRPC api, wip

---
+++ src/server/database/representation.ts
@@ -0,0 +1,27 @@
+import { Prisma, Representation } from "@prisma/client"
+import prisma from '../../client';
+
+
+export async function create(payload) {
+  console.log(`into representation create post`)
+  const { name, type, format, path, fileSize, assetId, uploaderId } = payload;
+  const representation: Prisma.RepresentationCreateInput = {
+    name: name,
+    type: type,
+    format: format,
+    path: path,
+    fileSize: fileSize,
+    asset: {
+      connect: {
+        id: assetId
+      }
+    },
+    uploader: {
+      connect: {
+        id: uploaderId
+      }
+    }
+  }
+  const result = await prisma.representation.create({ data: representation });
+  return result;
+}
