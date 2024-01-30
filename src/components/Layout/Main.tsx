1c1200b39ace5995a4e39adc1c773b58fa83d6b8
Noflame Lin <linjuang@gmail.com>
2024-01-31 04:17:38 +0800

    Add Galaxy Logo

--- src/components/Layout/Main.tsx
+++ src/components/Layout/Main.tsx
@@ -1,26 +1,26 @@
-import React, { useState } from 'react';
-import Typography from '@mui/material/Typography';
-import Header from '../Header';
-import { styled } from '@mui/material/styles';
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import React, { useState } from "react";
+import Typography from "@mui/material/Typography";
+import Header from "../Header";
+import { styled } from "@mui/material/styles";
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
 
-import MuiDrawer from '@mui/material/Drawer';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
+import MuiDrawer from "@mui/material/Drawer";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import Image from 'next/image'
 
-import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from "@mui/icons-material/Menu";
 
-import List from '@mui/material/List';
-import { MainListItems, TagListItems } from '../listItems';
+import List from "@mui/material/List";
+import { MainListItems, TagListItems } from "../listItems";
 
-
-import NextLink from 'next/link'
-import { Link as MUILink } from '@mui/material';
+import NextLink from "next/link";
+import { Link as MUILink } from "@mui/material";
 // import useSWR from "swr";
-import { MenuListItem } from '../../libs/types';
-import { trpc } from '../../utils/trpc';
+import { MenuListItem } from "../../libs/types";
+import { trpc } from "../../utils/trpc";
 
 const drawerWidth: number = 240;
 interface AppBarProps extends MuiAppBarProps {
@@ -28,47 +28,48 @@ interface AppBarProps extends MuiAppBarProps {
 }
 
 // const fetcher = (url: string) => fetch(url).then((r) => r.json());
-const AppBar = styled(MuiAppBar, { shouldForwardProp: (prop) => prop !== 'open', })<AppBarProps>(({ theme, open }) => ({
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})<AppBarProps>(({ theme, open }) => ({
   zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
+  transition: theme.transitions.create(["width", "margin"], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
   ...(open && {
     marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
+    transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
   }),
 }));
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
-    '& .MuiDrawer-paper': {
-      position: 'relative',
-      whiteSpace: 'nowrap',
-      width: drawerWidth,
-      transition: theme.transitions.create('width', {
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  "& .MuiDrawer-paper": {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    boxSizing: "border-box",
+    ...(!open && {
+      overflowX: "hidden",
+      transition: theme.transitions.create("width", {
         easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
+        duration: theme.transitions.duration.leavingScreen,
       }),
-      boxSizing: 'border-box',
-      ...(!open && {
-        overflowX: 'hidden',
-        transition: theme.transitions.create('width', {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.leavingScreen,
-        }),
-        width: theme.spacing(7),
-        [theme.breakpoints.up('sm')]: {
-          width: theme.spacing(9),
-        },
-      }),
-    },
-  }),
-);
-
+      width: theme.spacing(7),
+      [theme.breakpoints.up("sm")]: {
+        width: theme.spacing(9),
+      },
+    }),
+  },
+}));
 
 export default function Main({ children }: { children: React.ReactNode }) {
   const [open, setOpen] = useState(true);
@@ -77,18 +78,18 @@ export default function Main({ children }: { children: React.ReactNode }) {
   };
 
   const menuListQry = trpc.menus.all.useQuery();
-  if (menuListQry.isLoading || !menuListQry.isSuccess) return <div>Loading</div>
-  if (menuListQry.error) return <div>Error</div>
-  console.log(menuListQry.data.menus)
-
+  if (menuListQry.isLoading || !menuListQry.isSuccess)
+    return <div>Loading</div>;
+  if (menuListQry.error) return <div>Error</div>;
+  console.log(menuListQry.data.menus);
 
   return (
-    <Box sx={{ display: 'flex' }}>
-      <AppBar position="fixed" open={open} sx={{ boxShadow: 'none' }}>
+    <Box sx={{ display: "flex" }}>
+      <AppBar position="fixed" open={open} sx={{ boxShadow: "none" }}>
         <Toolbar
           sx={{
-            pr: '24px', // keep right padding when drawer closed
-            bgcolor: '#2F2F2F'
+            pr: "24px", // keep right padding when drawer closed
+            bgcolor: "#2F2F2F",
           }}
         >
           <IconButton
@@ -97,8 +98,8 @@ export default function Main({ children }: { children: React.ReactNode }) {
             aria-label="open drawer"
             onClick={toggleDrawer}
             sx={{
-              marginRight: '36px',
-              ...(open && { display: 'none' }),
+              marginRight: "36px",
+              ...(open && { display: "none" }),
             }}
           >
             <MenuIcon />
@@ -109,14 +110,13 @@ export default function Main({ children }: { children: React.ReactNode }) {
           </Box>
         </Toolbar>
       </AppBar>
-      <Drawer variant="permanent" open={open} sx={{ height: '100vh' }}>
+      <Drawer variant="permanent" open={open} sx={{ height: "100vh" }}>
         <Toolbar
           sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
             px: 1,
-
           }}
         >
           {/* <IconButton onClick={toggleDrawer}>
@@ -131,35 +131,51 @@ export default function Main({ children }: { children: React.ReactNode }) {
             {/* <Link href="/" style={{ textDecoration: 'none', color:'white' }}>
                 SMAUG
               </Link> */}
-            <NextLink href='/' passHref style={{ textDecoration: 'none' }}>
-              <MUILink variant="body2" underline="none" sx={{ transition: '.3s', color: 'white', fontSize: '1.3rem', fontWeight: 'bold', "&:hover": { color: '#eee', letterSpacing: '1px' } }} >SMAUG</MUILink>
+            <NextLink href="/" passHref style={{ textDecoration: "none" }}>
+              <MUILink
+                variant="body2"
+                underline="none"
+                sx={{
+                  transition: ".3s",
+                  color: "white",
+                  fontSize: "1.3rem",
+                  fontWeight: "bold",
+                  "&:hover": { color: "#eee", letterSpacing: "1px" },
+                }}
+              >
+                <Image src="/galaxy_logo_180px.png" alt="Galaxy" width={134} height={40} style={{ marginTop: "8px", marginLeft: "8px" }} />
+                {/* SMAUG */}
+              </MUILink>
             </NextLink>
           </Typography>
-
         </Toolbar>
         <Divider />
 
-        <List component="nav" sx={{
-          overflowY: 'auto',
-          '&::-webkit-scrollbar': {
-            width: '0.5em',
-            padding: 0,
-          },
-          '&::-webkit-scrollbar-thumb': {
-            padding: 0,
-            margin: 0,
-            width: '0.4em',
-            backgroundColor: '#555',
-            borderRadius: '2px'
-          }
-        }}>
-          {
-            menuListQry.data.menus.map((item: MenuListItem, index) => {
-              const { id, name } = item
-              return name === 'Tags' ? <TagListItems key={index} mainMenuData={item} /> : <MainListItems key={index} mainMenuData={item} />
-
-            })
-          }
+        <List
+          component="nav"
+          sx={{
+            overflowY: "auto",
+            "&::-webkit-scrollbar": {
+              width: "0.5em",
+              padding: 0,
+            },
+            "&::-webkit-scrollbar-thumb": {
+              padding: 0,
+              margin: 0,
+              width: "0.4em",
+              backgroundColor: "#555",
+              borderRadius: "2px",
+            },
+          }}
+        >
+          {menuListQry.data.menus.map((item: MenuListItem, index) => {
+            const { id, name } = item;
+            return name === "Tags" ? (
+              <TagListItems key={index} mainMenuData={item} />
+            ) : (
+              <MainListItems key={index} mainMenuData={item} />
+            );
+          })}
           {/* {Object.keys(mainListItem).length > 0  ?  <MainListItems  data={mainListItem} /> : <><LinearProgress /></>} */}
           {/* <TagListItems /> */}
           {/* <MemberListItems /> */}
@@ -169,17 +185,17 @@ export default function Main({ children }: { children: React.ReactNode }) {
         component="main"
         sx={{
           backgroundColor: (theme) =>
-            theme.palette.mode === 'light'
+            theme.palette.mode === "light"
               ? theme.palette.grey[100]
-              : '#242424',
+              : "#242424",
           flexGrow: 1,
-          height: '100vh',
-          overflow: 'auto',
+          height: "100vh",
+          overflow: "auto",
         }}
       >
         <Toolbar />
         {children}
       </Box>
     </Box>
-  )
+  );
 }
